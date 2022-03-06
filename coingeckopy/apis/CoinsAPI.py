5fbf8e32d812489539047d87cc7f3c3a9f845d7d
Adrian Hurtado <adhf94@gmail..com>
2022-03-06 11:03:11 -0400

    list function, pydantic install

--- coingeckopy/apis/CoinsAPI.py
+++ coingeckopy/apis/CoinsAPI.py
@@ -1,5 +1,15 @@
-RESOURCE_URL = "/coin/"
+import requests
+
+from coingeckopy.settings import API_URL
+
+RESOURCE_URL = "/coins"
 
 
 class CoinsAPI:
-    pass
+    def list(self):
+        r = requests.get(f"{API_URL}{RESOURCE_URL}/list/")
+        c = r.json()
+        print(c)
+
+coinl = CoinsAPI()
+coinl.list()
